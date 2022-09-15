581a1fe530c1f53e5edfba5abfc454d571d82336
Tor Hedin Brønner <torh@solutionseeker.no>
2022-09-15 20:53:56 +0200

    commitDetailView: wip extras

--- src/views/commitDetailView.ts
+++ src/views/commitDetailView.ts
@@ -7,21 +7,33 @@ import { MagitRepository } from '../models/magitRepository';
 import { MagitChange } from '../models/magitChange';
 import { ChangeView } from './changes/changeView';
 import { Section, SectionHeaderView } from './general/sectionHeader';
-import { ChangeSectionView } from './changes/changesSectionView';
 import { LineBreakView } from './general/lineBreakView';
+import GitTextUtils from '../utils/gitTextUtils';
+import { CommitItemView } from './commits/commitSectionView';
 
 export class CommitDetailView extends DocumentView {
 
   static UriPath: string = 'commit.magit';
-  isHighlightable = false;
+  isHighlightable = true;
   needsUpdate = false;
 
   constructor(uri: Uri, public commit: MagitCommit, changes: MagitChange[]) {
     super(uri);
 
+
+    this.addSubview(new CommitItemView(commit));
+    // this.addSubview(shaView);
+    const author = `${commit.authorName} <${commit.authorEmail}>`;
+    const parents = commit.parents.join(' ');
+    const authorDetails = new TextView(`Author:     ${author}`);
+    this.addSubview(authorDetails);
+
+    this.addSubview(new TextView(`AuthorDate: ${commit.authorDate}`));
+    this.addSubview(new TextView(`CommitDate: ${commit.commitDate}`));
+    this.addSubview(new TextView(`Parents:    ${parents}`));
+
     const messageView = new TextView(commit.message);
-    messageView.isHighlightable = true;
-    this.addSubview(messageView, new LineBreakView());
+    this.addSubview(new LineBreakView(), messageView, new LineBreakView());
 
     this.addSubview(
       ...changes.map(change => {
