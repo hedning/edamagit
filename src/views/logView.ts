5111328cc2e95352bfbc9b1a39536a7eb6a33f9d
Tor Hedin Brønner <torh@solutionseeker.no>
2024-01-20 11:27:15 +0100

    logView: use box drawing chars for graph
    The main win here is uninterrupted vertical lines.
    
    use ⏺ for commit
    
    logView: use ● for commit (not wide)
    
    logView: add box drawing help
    
    wip: rewrite for more graph context
    
    logView.prettifyGraph: use for i loop
    
    wip pretty graph
    
    handle star
    
    wip
    
    space rule
    
    wip bend
    
    wip bend out
    
    wip left bend
    
    wip left bend
    
    more bends
    
    more wip
    
    do defaults on / and \
    
    simplify
    
    starting to get there
    
    fix commit
    
    almost done
    
    star always connects with prev right /
    
    small fix
    
    logView: ascii fix up some comments
    
    wip
    
    logView: increase width
    
    logView: test out unlimited message length
    
    vscode doesn't give us available horizontal space, so if we can move
    all the constant sized things to the left, the message can get full size
    
    logView: ${hash}${truncatedAuthor}${truncatedTime}

--- src/views/logView.ts
+++ src/views/logView.ts
@@ -11,45 +11,239 @@ import { TextView } from './general/textView';
 import { Token } from './general/semanticTextView';
 import { SemanticTokenTypes } from '../common/constants';
 import { gitRun, LogLevel } from '../utils/gitRawRunner';
+import { Ref } from '../typings/git';
+import ViewUtils from '../utils/viewUtils';
+
+/** Box drawing chars
+    0 	1 	2 	3 	4 	5 	6 	7 	8 	9 	A 	B 	C 	D 	E 	F
+U+250x 	─ 	━ 	│ 	┃ 	┄ 	┅ 	┆ 	┇ 	┈ 	┉ 	┊ 	┋ 	┌ 	┍ 	┎ 	┏
+U+251x 	┐ 	┑ 	┒ 	┓ 	└ 	┕ 	┖ 	┗ 	┘ 	┙ 	┚ 	┛ 	├ 	┝ 	┞ 	┟
+U+252x 	┠ 	┡ 	┢ 	┣ 	┤ 	┥ 	┦ 	┧ 	┨ 	┩ 	┪ 	┫ 	┬ 	┭ 	┮ 	┯
+U+253x 	┰ 	┱ 	┲ 	┳ 	┴ 	┵ 	┶ 	┷ 	┸ 	┹ 	┺ 	┻ 	┼ 	┽ 	┾ 	┿
+U+254x 	╀ 	╁ 	╂ 	╃ 	╄ 	╅ 	╆ 	╇ 	╈ 	╉ 	╊ 	╋ 	╌ 	╍ 	╎ 	╏
+U+255x 	═ 	║ 	╒ 	╓ 	╔ 	╕ 	╖ 	╗ 	╘ 	╙ 	╚ 	╛ 	╜ 	╝ 	╞ 	╟
+U+256x 	╠ 	╡ 	╢ 	╣ 	╤ 	╥ 	╦ 	╧ 	╨ 	╩ 	╪ 	╫ 	╬ 	╭ 	╮ 	╯
+U+257x 	╰ 	╱ 	╲ 	╳ 	╴ 	╵ 	╶ 	╷ 	╸ 	╹ 	╺ 	╻ 	╼ 	╽ 	╾ 	╿
+*/
+
+const ascii = {
+  /**
+   * defaults to ╰
+   */
+  l: '\\',
+
+  /**
+   * defaults to ╯
+   */
+  r: '/',
+  up: '|',
+  star: '*',
+  pipe: '|',
+  _: '_',
+} as const;
+
+function prettifyGraph(prev: string, current: string, next: string): string {
+  // Consider using a string replace here
+  let out: string = '';
+  for (let i = 0; i < current.length; i++) {
+    const c = current[i];
+    const l = i - 1;
+    const r = i + 1;
+    switch (c) {
+      case '*': {
+        if (
+          (prev[i] === ascii.pipe || prev[i] === ascii.star) &&
+          (next[i] === ascii.pipe || next[i] === ascii.star)
+        ) { out += '┿'; break; }
+        if (
+          prev[r] === ascii.r &&
+          (next[i] === ascii.pipe || next[i] === ascii.star)
+        ) { out += '┿'; break; }
+        if (
+          prev[r] === ascii.r &&
+          next[i] !== ascii.pipe &&
+          next[i] !== ascii.star
+        ) { out += '┷'; break; }
+        if (
+          prev[i] === ascii.pipe || prev[i] === ascii.star
+        ) { out += '┷'; break; }
+        if (
+          next[i] === ascii.pipe || next[i] === ascii.star
+        ) { out += '┯'; break; }
+
+        out += '━'; break;
+      }
+      case '|': {
+        if ( // check if the / goes through us or not
+          current[r] === ascii.r &&
+          current[l] !== ascii._ &&
+          next[l] !== ascii.r
+        ) { out += '├'; break; }
+        if (
+          next[r] === ascii.l &&
+          next[i] === ' '
+        ) { out += '╰'; break; }
+        if (
+          next[l] === ascii.r &&
+          next[i] === ' '
+        ) { out += '╯'; break; }
+
+        out += '│'; break;
+      }
+      case ascii.r: {
+        if (
+          prev[r] === ascii.r &&
+          next[i] === ascii.pipe &&
+          current[r] === ' '
+        ) { out += '│'; break; }
+
+        out += '╯'; break;
+      }
+      case ascii.l: {
+        if (
+          prev[l] === ascii.star &&
+          prev[i] === ' ' &&
+          next[r] === ascii.star
+        ) { out += '│'; break; }
+
+        if ( // in-out
+          next[i] === ascii.r
+        ) { out += '│'; break; }
+
+        out += '╰'; break;
+      }
+      case ' ': {
+        if (
+          prev[i] === ascii.l &&
+          current[r] === ascii.star &&
+          next[i] === ascii.r
+        ) { out += '├'; break; }
+        if (
+          next[i] === ascii.l &&
+          current[l] === ascii.star
+        ) { out += '╮'; break; }
+        if (
+          prev[i] === ascii.l &&
+          current[r] === ascii.star
+        ) { out += '╰'; break; }
+        if (
+          (current[i + 2] === ascii.r || current[i + 2] === ascii._) &&
+          next[i] === ascii.r
+        ) { out += '╭'; break; }
+
+        if (
+          current[l] === ascii.pipe &&
+          next[i] === ascii.l &&
+          next[l] === ' '
+        ) { out += '╮'; break; }
+        if (
+          current[l] === ascii.l &&
+          next[i] === ascii.pipe
+          // next[l] === ' '
+        ) { out += '╮'; break; }
+        if (
+          current[l] === ascii.l &&
+          next[i] === ascii.l &&
+          next[l] === ' '
+        ) { out += '╮'; break; }
+
+        if (
+          current[r] === ascii.pipe &&
+          next[i] === ascii.r &&
+          next[r] === ' '
+        ) { out += '╭'; break; }
+        if (
+          current[r] === ascii.r &&
+          next[i] === ascii.r &&
+          next[r] === ' '
+        ) { out += '╭'; break; }
+        if (
+          current[r] === ascii.r &&
+          next[i] === ascii.pipe &&
+          next[r] === ' '
+        ) { out += '╭'; break; }
+
+        if (
+          current[r] === ascii.star &&
+          next[i] === ascii.r &&
+          next[l] === ascii.pipe
+        ) { out += '╭'; break; }
+        if (
+          current[r] === ascii.r &&
+          next[i] === ascii.star
+        ) { out += '╭'; break; }
+
+        out += ' '; break;
+      }
+      case ascii._: { out += '─'; break; }
+      default: { out += c; }
+    }
+  }
+  return out;
+}
 
 // regex to parse log lines
 const lineRe = new RegExp(
-  '^([/|\\-_* .o]+)?' + // Graph
+  '^([/|\\\\-_* .o]+)?' + // Graph
   '([a-f0-9]{40})' + // Sha
   '( \\(([^()]+)\\))?' + // Refs
   '( \\[([^\\[\\]]+)\\])' + // Author
   '( \\[([^\\[\\]]+)\\])' + // Time
   '(.*)$', // Message
   'g');
-const graphRe = /^[/|\\-_* .o]+$/g;
+const graphRe = /^[/|\\\\-_* .o]+$/g;
+function reParse(line: string): { graph: string } | { graph: string, refs: string, author: string, time: string, hash: string, message: string } {
+  if (!line) return { graph: '' };
+  if (line.match(graphRe)) return { graph: line };
+
+  const matches: string[] = line.matchAll(lineRe).next().value;
+  if (!matches) return { graph: '' };
+  return {
+    graph: matches[1]!,
+    refs: matches[4]!,
+    author: matches[6]!,
+    time: matches[8]!, // convert seconds to milliseconds
+    hash: matches[2]!,
+    message: matches[9]!,
+  };
+}
 
 function parseLog(stdout: string): MagitLogEntry[] {
   const lines = stdout.match(/[^\r\n]+/g);
   if (!lines) return [];
 
+  if (lines.length < 2) return [];
+
+  let prev = { graph: '' };
+  let i = 0;
+  let current = reParse(lines[i]);
+  i += 1;
+  let next = reParse(lines[i]) ?? { graph: '' };
   const commits: MagitLogEntry[] = [];
-  for (const line of lines) {
-    if (line.match(graphRe)) { // graph only, ie. the whole line is just graph stuff
-      commits[commits.length - 1]?.graph?.push(line);
-      continue;
+  while (i <= lines.length) {
+    const graph = prettifyGraph(prev.graph, current.graph, next.graph);
+
+    if ('refs' in current) {
+      commits.push({
+        graph: graph ? [graph] : undefined,
+        refs: (current.refs ?? '').split(', ').filter((m: string) => m),
+        author: current.author,
+        time: new Date(Number(current.time) * 1000), // convert seconds to milliseconds
+        commit: {
+          hash: current.hash,
+          message: current.message,
+          parents: [],
+          authorEmail: undefined
+        }
+      });
+    } else {
+      commits[commits.length - 1]?.graph?.push(graph);
     }
 
-    const matches = line.matchAll(lineRe).next().value;
-    if (!matches || matches.length === 0) continue;
-
-    const graph = matches[1]; // undefined if graph doesn't exist
-    commits.push({
-      graph: graph ? [graph] : undefined,
-      refs: (matches[4] ?? '').split(', ').filter((m: string) => m),
-      author: matches[6],
-      time: new Date(Number(matches[8]) * 1000), // convert seconds to milliseconds
-      commit: {
-        hash: matches[2],
-        message: matches[9],
-        parents: [],
-        authorEmail: undefined
-      }
-    });
+    i += 1;
+    prev = current;
+    current = next;
+    next = reParse(lines[i]);
   }
 
   return commits;
@@ -75,12 +269,24 @@ export default class LogView extends DocumentView {
   public async update(state: MagitRepository) {
     const repo = state.gitRepository;
     const output = await gitRun(repo, this.args.concat(this.revs, ['--'], this.paths), {}, LogLevel.Error);
+    const refs = state.remotes.reduce((prev, remote) => remote.branches.concat(prev), state.branches.concat(state.tags));
+
+
+    let defaultBranches: { [remoteName: string]: string } = {};
+    for await (const remote of state.remotes) {
+      try {
+        let defaultBranch = await gitRun(state.gitRepository, ['symbolic-ref', `refs/remotes/${remote.name}/HEAD`], undefined, LogLevel.Error);
+        defaultBranches[remote.name] = defaultBranch.stdout.replace(`refs/remotes/${remote.name}/`, '').trimEnd();
+      } catch { } // gitRun will throw an error if remote/HEAD doesn't exist - we do not need to do anything in this case
+    }
+
     const logEntries = parseLog(output.stdout);
     const revName = this.revs.join(' ');
 
     this.subViews = [
       new TextView(`Commits in ${revName}`),
-      ...logEntries.map(entry => new CommitLongFormItemView(entry)),
+      // ...logEntries.map(entry => new CommitLongFormItemView(entry, refs)),
+      ...logEntries.map(entry => new CommitLongFormItemView(entry, refs, state.HEAD?.name, defaultBranches)),
     ];
     // For some reason the fire event can get eaten if fired synchronously
     const trigger = () => {
@@ -100,8 +306,8 @@ export default class LogView extends DocumentView {
 
 export class CommitLongFormItemView extends CommitItemView {
 
-  constructor(public logEntry: MagitLogEntry) {
-    super(logEntry.commit);
+  constructor(public logEntry: MagitLogEntry, refs?: Ref[], headName?: string, defaultBranches?: { [remoteName: string]: string }) {
+    super(logEntry.commit, undefined, refs);
 
     const timeDistance = formatDistanceToNowStrict(logEntry.time);
     const hash = `${GitTextUtils.shortHash(logEntry.commit.hash)} `;
@@ -110,21 +316,8 @@ export class CommitLongFormItemView extends CommitItemView {
     this.content = [];
 
     const msg = GitTextUtils.shortCommitMessage(logEntry.commit.message);
-    this.content.push(`${hash}${graph}`);
 
-    const refTokens: Token[] = logEntry.refs.map(ref => new Token(ref, SemanticTokenTypes.RefName));
-    if (refTokens.length) {
-
-      this.content.push(' (');
-      refTokens.forEach(refToken => {
-        this.content.push(refToken, ' ');
-      });
-      this.content.pop();
-
-      this.content.push(') ');
-    }
-
-    const availableMsgWidth = 70 - this.content.reduce((prev, v) => prev + v.length, 0);
+    const availableMsgWidth = 120 - this.content.reduce((prev, v) => prev + v.length, 0);
     let maxAuthorWidth = 17;
     // fixme: add setting toggle
     const shortenToInitials = true;
@@ -133,16 +326,23 @@ export class CommitLongFormItemView extends CommitItemView {
       logEntry.author = initials;
       maxAuthorWidth = 4;
     }
-
     const truncatedAuthor = truncateText(logEntry.author, maxAuthorWidth, maxAuthorWidth + 1);
-    const truncatedMsg = truncateText(msg, availableMsgWidth, availableMsgWidth + 1);
-    this.content.push(`${truncatedMsg}${truncatedAuthor}${timeDistance}`);
+    const truncatedTime = truncateText(timeDistance, 9, 9 + 1);
+    const preamble = `${hash}${truncatedAuthor}${truncatedTime}`;
+    this.content.push(`${preamble}${graph}`);
+    if (logEntry.refs.length) {
+      this.content.push(...ViewUtils.generateRefTokensLine(logEntry.commit.hash, refs, headName, defaultBranches));
+    }
+
+    const truncatedMsg = msg;
+    // const truncatedMsg = truncateText(msg, availableMsgWidth, availableMsgWidth + 1);
+    this.content.push(truncatedMsg);
 
     // Add the rest of the graph for this commit
     if (logEntry.graph) {
       for (let i = 1; i < logEntry.graph.length; i++) {
         const g = logEntry.graph[i];
-        const emptyHashSpace = ' '.repeat(8);
+        const emptyHashSpace = ' '.repeat(preamble.length);
         this.content.push(`\n${emptyHashSpace}${g}`);
       }
     }
