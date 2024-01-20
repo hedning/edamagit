78b380e29feef1b45f2c86222af4107d91c48cf9
Tor Hedin Brønner <torh@solutionseeker.no>
2024-01-21 00:45:42 +0100

    resettingCommands: make thenable

--- src/commands/resettingCommands.ts
+++ src/commands/resettingCommands.ts
@@ -1,14 +1,21 @@
-import { MenuState, MenuUtil } from '../menu/menu';
+import { Menu, MenuState, MenuUtil } from '../menu/menu';
 import { MagitRepository } from '../models/magitRepository';
 import { gitRun } from '../utils/gitRawRunner';
 import MagitUtils from '../utils/magitUtils';
 import { window } from 'vscode';
 
-const resettingMenu = {
+const resettingMenu: Menu = {
   title: 'Resetting',
   commands: [
     { label: 'm', description: 'reset mixed (HEAD and index)', action: ({ repository }: MenuState) => resetMixed(repository) },
-    { label: 's', description: 'reset soft (HEAD only)', action: ({ repository }: MenuState) => _reset(repository, ['--soft'], `Soft reset ${repository.HEAD?.name} to`) },
+    {
+      label: 's', description: 'reset soft (HEAD only)', action: async ({ repository }: MenuState) => {
+        const repo = await repository;
+        if (!repo) return;
+
+        _reset(repository, ['--soft'], `Soft reset ${repo.HEAD?.name} to`);
+      }
+    },
     { label: 'h', description: 'reset hard (HEAD, index and files)', action: ({ repository }: MenuState) => resetHard(repository) },
     { label: 'i', description: 'reset index (only)', action: ({ repository }: MenuState) => _reset(repository, [], `Reset index to`) },
     { label: 'w', description: 'reset worktree (only)', action: resetWorktree }
@@ -16,35 +23,45 @@ const resettingMenu = {
   ]
 };
 
-export async function resetting(repository: MagitRepository) {
+export async function resetting(repository: Thenable<MagitRepository | undefined>) {
   return MenuUtil.showMenu(resettingMenu, { repository });
 }
 
-export async function resetMixed(repository: MagitRepository) {
-  return _reset(repository, ['--mixed'], `Reset ${repository.HEAD?.name} to`);
+export async function resetMixed(repository: Thenable<MagitRepository | undefined>) {
+  const repo = await repository;
+  if (!repo) return;
+
+  return _reset(repository, ['--mixed'], `Reset ${repo.HEAD?.name} to`);
 }
 
-export async function resetHard(repository: MagitRepository) {
-  return _reset(repository, ['--hard'], `Hard reset ${repository.HEAD?.name} to`);
+export async function resetHard(repository: Thenable<MagitRepository | undefined>) {
+  const repo = await repository;
+  if (!repo) return;
+
+  return _reset(repository, ['--hard'], `Hard reset ${repo.HEAD?.name} to`);
 }
 
 async function resetWorktree({ repository }: MenuState) {
+  const repo = await repository;
+  if (!repo) return;
 
-  const ref = await window.showQuickPick([`${repository.HEAD?.name}`, 'HEAD'], { placeHolder: 'Reset worktree to' });
+  const ref = await window.showQuickPick([`${repo.HEAD?.name}`, 'HEAD'], { placeHolder: 'Reset worktree to' });
 
   if (ref) {
     const args = ['checkout-index', '--all', '--force'];
-    return await gitRun(repository.gitRepository, args);
+    return await gitRun(repo.gitRepository, args);
   }
 }
 
-async function _reset(repository: MagitRepository, switches: string[], prompt: string) {
+async function _reset(repository: Thenable<MagitRepository | undefined>, switches: string[], prompt: string) {
+  const repo = await repository;
+  if (!repo) return;
 
-  const ref = await MagitUtils.chooseRef(repository, prompt, true, true);
+  const ref = await MagitUtils.chooseRef(repo, prompt, true, true);
 
   if (ref) {
 
     const args = ['reset', ...switches, ref];
-    return await gitRun(repository.gitRepository, args);
+    return await gitRun(repo.gitRepository, args);
   }
 }
