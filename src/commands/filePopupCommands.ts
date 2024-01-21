c3cec544dd857da694f0106765cbc56c6716b4bd
Tor Hedin Brønner <torh@solutionseeker.no>
2024-01-21 12:21:52 +0100

    filePopupCommands: make thenable

--- src/commands/filePopupCommands.ts
+++ src/commands/filePopupCommands.ts
@@ -10,21 +10,58 @@ import * as Logging from './loggingCommands';
 const filePopupMenu = {
   title: 'File Actions',
   commands: [
-    { label: 's', description: 'Stage', action: ({ repository, data }: MenuState) => Staging.stageFile(repository, data as Uri) },
-    { label: 'u', description: 'Unstage', action: ({ repository, data }: MenuState) => Staging.unstageFile(repository, data as Uri) },
-    { label: 'c', description: 'Commit', action: ({ repository }: MenuState) => Commit.magitCommit(repository) },
+    {
+      label: 's', description: 'Stage', action: async ({ repository, data }: MenuState) => {
+        const repo = await repository;
+        if (!repo) return;
+
+        Staging.stageFile(repo, data as Uri);
+      }
+    },
+    {
+      label: 'u', description: 'Unstage', action: async ({ repository, data }: MenuState) => {
+        const repo = await repository;
+        if (!repo) return;
+
+        Staging.unstageFile(repo, data as Uri);
+      }
+    },
+    { label: 'c', description: 'Commit', action: async ({ repository }: MenuState) => Commit.magitCommit(repository) },
     // { label: 'D', description: 'Diff...', action: () => { } },
-    { label: 'd', description: 'Diff', action: ({ repository, data }: MenuState) => Diffing.diffFile(repository, data as Uri) },
+    {
+      label: 'd', description: 'Diff', action: async ({ repository, data }: MenuState) => {
+        const repo = await repository;
+        if (!repo) return;
+
+        Diffing.diffFile(repo, data as Uri);
+      }
+    },
     // { label: 'L', description: 'Log...', action: () => { } },
-    { label: 'l', description: 'log', action: ({ repository, data }: MenuState) => Logging.logFile(repository, data as Uri)  },
+    {
+      label: 'l', description: 'log', action: async ({ repository, data }: MenuState) => {
+        const repo = await repository;
+        if (!repo) return;
+
+        Logging.logFile(repo, data as Uri);
+      }
+    },
     // { label: 't', description: 'trace', action: () => { } },
     // { label: 'B', description: 'Blame...', action: () => { } },
-    { label: 'b', description: 'Blame', action: ({ repository, data }: MenuState) => Blaming.blameFile(repository, data as Uri) },
+    {
+      label: 'b', description: 'Blame', action: async ({ repository, data }: MenuState) => {
+        const repo = await repository;
+        if (!repo) return;
+
+        Blaming.blameFile(repo, data as Uri);
+      }
+    },
     // { label: 'n', description: 'prev blob', action: () => { } },
     // { label: 'n', description: 'next blob', action: () => { } }
   ]
 };
 
+
 export async function filePopup(repository: MagitRepository, fileUri: Uri) {
-  return MenuUtil.showMenu(filePopupMenu, { repository, data: fileUri });
+  const promise = new Promise<MagitRepository>((res, rej) => { res(repository); });
+  return MenuUtil.showMenu(filePopupMenu, { repository: promise, data: fileUri });
 }
