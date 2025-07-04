6ce5d8da8e100388e7852431bf6e9817e371376e
Tor Hedin Brønner <torh@solutionseeker.no>
2025-07-04 17:04:10 +0200

    logview: put the revs in the title

--- src/views/logView.ts
+++ src/views/logView.ts
@@ -362,7 +362,7 @@ export default class LogView extends DocumentView {
   }
 
   static encodeLocation(repository: MagitRepository, revs: string[], args: string[]): Uri {
-    return Uri.parse(`${Constants.MagitUriScheme}:${LogView.UriPath}?${repository.uri.fsPath}#${revs.join(':')}:${args.join('&')}`);
+    return Uri.parse(`${Constants.MagitUriScheme}:Log: ${revs.join(' ')}.magit?${repository.uri.fsPath}#${args.join('&')}`);
   }
 }
 
