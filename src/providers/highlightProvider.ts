b36b79cbca769b7e634f00de162a701a0296dd20
Tor Hedin Brønner <torh@solutionseeker.no>
2025-08-30 17:05:37 +0200

    Make section highlight the first line of a view range
    This is less distracting, and there's only 3 choices of highlights
    we can pick from, so choosing one with less contrast isn't necessarily
    viable.
    
    The highlight does flash though, which looks like a bug when the cursor
    is outside the highlight...

--- src/providers/highlightProvider.ts
+++ src/providers/highlightProvider.ts
@@ -17,6 +17,15 @@ export default class HighlightProvider implements vscode.DocumentHighlightProvid
     const clickedView = currentView.click(position);
     if (!clickedView?.isHighlightable) return;
 
-    return [new vscode.DocumentHighlight(clickedView.range, vscode.DocumentHighlightKind.Text)];
+    // Ideally we probably want the default word highlighting here? But turning this off results in really awful highlighting.
+
+    // Some notes:
+    // vscode won't ask for new highlights if the cursor stays inside a highlight range, and for 
+    // some reason they proactively kill highlights when you move (this might make sense if highlighting is slow) resulting
+    // in a blinking header highlight, which get really annoying
+    // But by highlighting the line and the header we can easily F7 between them, though ctrl+up and undo cursor basically does that already
+    const header = document.lineAt(clickedView.range.start).range
+    const line = document.lineAt(position.line).range;
+    return [new vscode.DocumentHighlight(header, vscode.DocumentHighlightKind.Text), new vscode.DocumentHighlight(line, vscode.DocumentHighlightKind.Text)];
   }
 }
